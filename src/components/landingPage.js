7157935747321e799f2927df7d544620aebac5c4
tajohnson245 <122384998+tajohnson245@users.noreply.github.com>
2024-03-16 09:55:59 -0400

    Add header jsx

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,9 +1,33 @@
 import React from "react";
+import BYTEFLOW_LOGO from "../logos/BYTEFLOW_LOGO.png";
 
 const landingpage = () => {
   return (
-    <div>
-      <h1>THIS IS THE LANDING PAGE</h1>
+    <div className="landing-page">
+      <div className="header">
+        <img
+          src={BYTEFLOW_LOGO}
+          alt="BYTEFLOW LOGO"
+          style={{ width: "300px", height: "auto" }}
+          className="byteflow-logo"
+        />
+        <nav>
+          <ul>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/ourTeam">Our Team</a>
+            </li>
+            <li>
+              <a href="/ourSolutions">Solutions</a>
+            </li>
+          </ul>
+        </nav>
+        <a>
+          <button>CONNECT</button>
+        </a>
+      </div>
     </div>
   );
 };
