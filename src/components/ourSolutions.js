fa88f84b8f5fea262a8d7b39fb03ed597f2f4da8
tajohnson245 <tajohnson245@outlook.com>
2024-04-01 19:07:16 -0400

    Add boilerplate code for ourSolutions and ourTeam pages

---
+++ src/components/ourSolutions.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ourSolutions = () => {
+  return <h1>This is the Solutions Page</h1>;
+};
+
+export default ourSolutions;
---
+++ src/components/ourTeam.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ourTeam = () => {
+  return <h1>This is the Our Team Page</h1>;
+};
+
+export default ourTeam;
