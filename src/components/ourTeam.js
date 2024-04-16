b27b8b45b2bf0ef07ad2a0f4bc853b8bfef00757
tajohnson245 <tajohnson245@outlook.com>
2024-04-15 23:21:39 -0400

    Add intro box header

--- src/components/ourTeam.js
+++ src/components/ourTeam.js
@@ -4,30 +4,45 @@ import "../styles/ourTeam.css";
 
 const ourTeam = () => {
   return (
-    <div className="header">
-      <img
-        src={BYTEFLOW_LOGO}
-        alt="BYTEFLOW LOGO"
-        style={{ width: "300px", height: "90px" }}
-        className="byteflow-logo"
-      />
-      <nav className="nav_links">
-        <ul>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/ourTeam">Our Team</a>
-          </li>
-          <li>
-            <a href="/ourSolutions">Solutions</a>
-          </li>
-        </ul>
-      </nav>
-      <a>
-        <button className="connect-btn">CONNECT</button>
-      </a>
-    </div>
+    <body>
+      <div className="header">
+        <img
+          src={BYTEFLOW_LOGO}
+          alt="BYTEFLOW LOGO"
+          style={{ width: "300px", height: "90px" }}
+          className="byteflow-logo"
+        />
+        <nav className="nav_links">
+          <ul>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/ourTeam">Our Team</a>
+            </li>
+            <li>
+              <a href="/ourSolutions">Solutions</a>
+            </li>
+          </ul>
+        </nav>
+        <a>
+          <button className="connect-btn">CONNECT</button>
+        </a>
+      </div>
+
+      <div className="box">
+        <h1 className="text1">Meet The Team.</h1>
+        <div className="text-box">
+          <h3 className="text2">
+            We are not just a team; we're a force of digital innovation. With
+            tech visionaries, data wizards, and design maestros, we sculpt
+            seamless user experiences and redefine what's possible. Beyond work,
+            we're adventurers and artists, fueling our passions as fiercely as
+            our projects."
+          </h3>
+        </div>
+      </div>
+    </body>
   );
 };
 
